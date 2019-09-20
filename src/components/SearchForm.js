c5047007efd5f71a833729c954487ea43674f121
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-09-20 11:56:27 -0600

    final commit before lunch

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import axios from "axios";
 export default function SearchForm() {
  
+
   return (
     <section className="search-form">
-     
-     
-     // Add a search form here
+      // Add a search form here
     </section>
   );
 }
